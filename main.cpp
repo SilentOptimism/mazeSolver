a62af8994e967704ede2323e2a3d35bd0208d098
Joey Dhillon <imjoeydhillon@gmail.com>
2024-11-22 16:44:31 -0800

    commit 2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include <iostream>
-
 using std::cout;
 using std::endl;
 
+#include <array>
+using std::array;
+
+// Start (0,0)
+// end (9,8)
+array<array<int, 10>,10> maze = {{
+    {0, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 1, 0, 0, 0, 0, 1},
+    {1, 1, 1, 0, 1, 0, 1, 1, 0, 1},
+    {1, 1, 1, 0, 1, 0, 1, 1, 0, 1},
+    {1, 1, 1, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1, 0, 1},
+    {1, 1, 1, 1, 1, 1, 0, 1, 0, 0},
+    {1, 1, 1, 1, 1, 1, 0, 0, 0, 1}
+}};
+
+
+
+void print(){
+    for(array<int,10>& row : maze){
+        for(int element: row){
+            cout << element << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main(int argc, char const *argv[])
 {
-    cout << "test" << endl;
+    print();
 
     return 0;
 }
